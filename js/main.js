a4613b7d704c1c4379ffdb18250b7d293560de5c
gregsakai <gregsakai@gmail.com>
2018-01-16 19:28:09 -0800

    commented js, fixed v/h offset

--- js/main.js
+++ js/main.js
@@ -1,19 +1,31 @@
 var button = document.getElementById("button");
-button.style.width = "100px";
+var scoreDiv = document.getElementById("scoreDiv");
+var score = 0;
 
-var hMax = window.innerWidth;
-var vMax = window.innerHeight;
+// Styled this part in JavaScript to reference it within the function
+button.style.width = "100px";
+button.style.height = "50px";
+
+var hWindowSize = window.innerWidth;
+var vWindowSize = window.innerHeight;
 
 function randomizer() {
-  var hRandom = Math.round(Math.random()*hMax);
-  var vRandom = Math.round(Math.random()*vMax);
+  // Random value between 0 and the dimensions of the browser window
+  var hRandom = Math.round(Math.random()*hWindowSize);
+  var vRandom = Math.round(Math.random()*vWindowSize);
+
   console.log("Horizontal: "+hRandom);
   console.log("Vertical: "+vRandom);
+
   button.style.marginLeft = hRandom+"px";
   button.style.marginTop = vRandom+"px";
 
-  var hMaxOffset = hMax-parseInt(button.style.width);
-  if(hRandom > hMaxOffset){
+  // Maximum left offset with "mole" size taken into account
+  var hMax = hWindowSize-parseInt(button.style.width);
+  var vMax = vWindowSize-parseInt(button.style.height);
+
+  // If the "mole" goes outside the bounds of the window, re-run the function
+  if(hRandom > hMax || vRandom > vMax){
     console.log("Off the edge, reset");
     randomizer();
   }
@@ -23,9 +35,6 @@ function randomizer() {
 setInterval(randomizer, 10000);
 
 // SCOREKEEPING FUNCTIONALITY
-
-var scoreDiv = document.getElementById("scoreDiv");
-var score = 0;
 
 button.addEventListener("click", function() {
   score++;
